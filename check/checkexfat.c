9daed23159efa408dc2fcd557efc583c0f9a9cf5
LeavaTail <starbow.duster@gmail.com>
2021-05-06 00:07:16 +0900

    [Add] check: Add Allocation Bitmap checker

--- check/checkexfat.c
+++ check/checkexfat.c
@@ -67,6 +67,79 @@ static void version(const char *command_name, const char *version, const char *a
 	fprintf(stdout, "Written by %s.\n", author);
 }
 
+/**
+ * exfat_set_bitmap - set bit at @clu in @b
+ * @b                 Allocation bitmap cache
+ * @clu               cluster
+ *
+ * @return             0 (success)
+ *                    -EINVAL (already allocated)
+ */
+static int exfat_set_bitmap(uint8_t *b, uint32_t clu)
+{
+	int ret = 0;
+	int offset, byte;
+	uint8_t mask = 0x01;
+
+	byte = (clu - EXFAT_FIRST_CLUSTER) / CHAR_BIT;
+	offset = (clu - EXFAT_FIRST_CLUSTER) % CHAR_BIT;
+	mask <<= offset;
+
+	if (b[byte] & mask) {
+		pr_warn("Cluster#%u is referenced from other cluster.\n", clu);
+		ret = -EINVAL;
+	} else {
+		b[byte] = b[byte] | mask;
+	}
+
+	return ret;
+}
+
+/**
+ * exfat_set_reserve_bitmap - set bit at Allocation bitmap, Upcase-table, Root Directory in @b
+ * @b                         Allocation bitmap cache
+ * @clu                       cluster index
+ * @len                       data length
+ *
+ * @return            0 (success)
+ */
+static int exfat_set_reserved_bitmap(uint8_t *b, uint32_t clu, uint64_t len)
+{
+	int ret = 0;
+	struct exfat_fileinfo f = {0};
+
+	f.clu = clu;
+	f.datalen = len;
+
+	while (clu != 0 && clu != EXFAT_LASTCLUSTER) {
+		ret = exfat_set_bitmap(b, clu);
+		clu = exfat_next_cluster(&f, clu);
+	}
+	return ret;
+}
+
+/**
+ * exfat_check_bitmap - check whether Freed cluster is fine in Allocation bitmap 
+ * @b                   Allocation bitmap cache
+ *
+ * @return            0 (success)
+ */
+static int exfat_check_bitmap(uint8_t *b)
+{
+	int i, j;
+	int ret = 0;
+
+	for (i = 0; i < info.cluster_count / CHAR_BIT; i++) {
+		for (j = 0; j < CHAR_BIT; j++) {
+			if (((b[i] >> j) & 0x01) ^ ((info.alloc_table[i] >> j) & 0x01)) {
+				pr_warn("Cluster#%u isn't used at all.\n", EXFAT_FIRST_CLUSTER + i * CHAR_BIT + j);
+				ret = -EINVAL;
+			}
+		}
+	}
+	pr_msg("\n");
+	return ret;
+}
 
 /**
  * main   - main function
@@ -82,6 +155,8 @@ int main(int argc, char *argv[])
 	uint32_t clu = 0;
 	struct exfat_bootsec boot;
 	uint8_t *alloc_table = NULL;
+	node2_t *tmp;
+	struct exfat_fileinfo *f;
 
 	while ((opt = getopt_long(argc, argv,
 					"",
@@ -130,18 +205,32 @@ int main(int argc, char *argv[])
 	/* Ignore errot message in Root Directory */
 	if (exfat_traverse_root_directory())
 		goto out;
+	f = (struct exfat_fileinfo *)info.root[0]->data;
 
 	alloc_table = calloc(info.cluster_size, 1);
 	if (!alloc_table)
 		goto fat_free;
 
+	exfat_set_reserved_bitmap(alloc_table, info.alloc_offset, info.alloc_length);
+	exfat_set_reserved_bitmap(alloc_table, info.upcase_offset, info.upcase_size);
+	exfat_set_reserved_bitmap(alloc_table, info.root_offset, f->datalen);
+
 	for (i = 0; i < info.root_size && info.root[i]; i++) {
 		exfat_traverse_directory(info.root[i]->index);
-		clu = info.root[i]->index;
-		for (clu = info.root[i]->index; clu != 0 && clu != EXFAT_LASTCLUSTER; clu = exfat_next_cluster(info.root[i]->data, clu))
-			if (!exfat_load_bitmap(clu))
-				pr_err("Cluster #%u is not allocated, but directory use this cluster.\n", clu);
+		tmp = info.root[i];
+		clu = tmp->index;
+		/* Traverse directory chain */
+		while (tmp->next != NULL) {
+			tmp = tmp->next;
+			f = (struct exfat_fileinfo *)tmp->data;
+			/* File */
+			for (clu = tmp->index;
+					clu != 0 && clu != EXFAT_LASTCLUSTER;
+					clu = exfat_next_cluster(f, clu))
+				exfat_set_bitmap(alloc_table, clu);
+		}
 	}
+	exfat_check_bitmap(alloc_table);
 	exfat_print_cache();
 
 	ret = EXIT_SUCCESS;
