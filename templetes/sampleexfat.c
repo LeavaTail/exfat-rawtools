e3b05c3fd3bddeaa8fee6af7ae7c4cd2a10b57cb
LeavaTail <starbow.duster@gmail.com>
2021-05-09 10:30:51 +0900

    [Add] common: how to contribute

---
+++ templetes/sampleexfat.c
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ *  Copyright (C) YEAR AUTHOR
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <limits.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <mntent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "exfat.h"
+#include "sampleexfat.h"
+
+FILE *output;
+unsigned int print_level = PRINT_WARNING;
+struct exfat_info info;
+
+/**
+ * Special Option(no short option)
+ */
+enum
+{
+	GETOPT_HELP_CHAR = (CHAR_MIN - 2),
+	GETOPT_VERSION_CHAR = (CHAR_MIN - 3)
+};
+
+/* option data {"long name", needs argument, flags, "short name"} */
+static struct option const longopts[] =
+{
+	{"help", no_argument, NULL, GETOPT_HELP_CHAR},
+	{"version", no_argument, NULL, GETOPT_VERSION_CHAR},
+	{0,0,0,0}
+};
+
+/**
+ * usage - print out usage
+ */
+static void usage(void)
+{
+	fprintf(stderr, "Usage: %s [OPTION]...\n", PROGRAM_NAME);
+	fprintf(stderr, "DESCRIPTION\n");
+	fprintf(stderr, "\n");
+
+	fprintf(stderr, "  --help\tDESCRIPTION.\n");
+	fprintf(stderr, "  --version\toutput version information and exit.\n");
+	fprintf(stderr, "\n");
+}
+
+/**
+ * version        - print out program version
+ * @command_name:   command name
+ * @version:        program version
+ * @author:         program authoer
+ */
+static void version(const char *command_name, const char *version, const char *author)
+{
+	fprintf(stdout, "%s %s\n", command_name, version);
+	fprintf(stdout, "\n");
+	fprintf(stdout, "Written by %s.\n", author);
+}
+
+/**
+ * main   - main function
+ * @argc:   argument count
+ * @argv:   argument vector
+ */
+int main(int argc, char *argv[])
+{
+	int i, index;
+	int opt;
+	int longindex;
+	int ret = -EINVAL;
+	struct exfat_bootsec boot;
+
+	while ((opt = getopt_long(argc, argv,
+					"",
+					longopts, &longindex)) != -1) {
+		switch (opt) {
+			case GETOPT_HELP_CHAR:
+				usage();
+				exit(EXIT_SUCCESS);
+			case GETOPT_VERSION_CHAR:
+				version(PROGRAM_NAME, PROGRAM_VERSION, PROGRAM_AUTHOR);
+				exit(EXIT_SUCCESS);
+			default:
+				usage();
+				exit(EXIT_FAILURE);
+		}
+	}
+
+#ifdef EXFAT_DEBUG
+	print_level = PRINT_DEBUG;
+#endif
+
+	if (optind != argc - 1) {
+		usage();
+		exit(EXIT_FAILURE);
+	}
+
+	output = stdout;
+	if (exfat_init_info())
+		goto out;
+
+	if ((info.fd = open(argv[optind], O_RDONLY)) < 0) {
+		pr_err("open: %s\n", strerror(errno));
+		ret = -EIO;
+		goto out;
+	}
+
+	if (exfat_load_bootsec(&boot))
+		goto out;
+	if (exfat_store_info(&boot))
+		goto out;
+	if (exfat_traverse_root_directory())
+		goto out;
+
+	ret = EXIT_SUCCESS;
+
+out:
+	exfat_clean_info();
+	return ret;
+}
---
+++ templetes/sampleexfat.h
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ *  Copyright (C) YEAR AUTHOR
+ */
+#ifndef _SAMPLEEXFAT_H
+#define _SAMPLEEXFAT_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+/**
+ * Program Name, version, author.
+ * displayed when 'usage' and 'version'
+ */
+#define PROGRAM_NAME     "sampleexfat"
+#define PROGRAM_VERSION  "0.1.0"
+#define PROGRAM_AUTHOR   "AUTHOR"
+#define COPYRIGHT_YEAR   "YEAR"
+
+#endif /*_DIFFEXFAT_H */
