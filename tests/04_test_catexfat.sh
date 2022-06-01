1d34b48b082f3d9144419c0d4f4029c4e0742e73
LeavaTail <starbow.duster@gmail.com>
2022-06-01 10:57:41 +0900

    [Improve] common: Add testcase for error case

--- tests/04_test_catexfat.sh
+++ tests/04_test_catexfat.sh
@@ -2,6 +2,7 @@
 
 PROG=./catexfat
 IMAGE=exfat.img
+RET=0
 
 set -eu -o pipefail
 trap 'echo "ERROR: l.$LINENO, exit status = $?" >&2; exit 1' ERR
@@ -12,3 +13,54 @@ ${PROG} ${IMAGE} /0_SIMPLE/FILE.TXT
 ### Option function ###
 ${PROG} --help
 ${PROG} --version
+
+### Error path ###
+
+# Failure argument verification
+${PROG} ${IMAGE} 0 0 0 || RET=$?
+if [ $RET -eq 0 ]; then
+	echo "ERROR: Argument verification may be wrong"
+fi
+RET=0
+
+# Failure parse verification
+${PROG} -z ${IMAGE} / || RET=$?
+if [ $RET -eq 0 ]; then
+	echo "ERROR: Option Parser verification may be wrong"
+fi
+RET=0
+
+# Failure exist verification
+${PROG} nothing.img / || RET=$?
+if [ $RET -eq 0 ]; then
+	echo "ERROR: Open file verification may be wrong"
+fi
+RET=0
+
+# Failure exFAT image verification
+${PROG} README.md / || RET=$?
+if [ $RET -eq 0 ]; then
+	echo "ERROR: Image verification may be wrong"
+fi
+RET=0
+
+# Failure Directory verification
+${PROG} ${IMAGE} /0_SIMPLE || RET=$?
+if [ $RET -eq 0 ]; then
+	echo "ERROR: Directory verification may be wrong"
+fi
+RET=0
+
+# Failure File/Directory verification
+${PROG} ${IMAGE} /0_SIMPLE/FILE.TXT/ || RET=$?
+if [ $RET -eq 0 ]; then
+	echo "ERROR: File or Directory verification may be wrong"
+fi
+RET=0
+
+# Failure traverse verification
+${PROG} ${IMAGE} /NOTHING/NONE/NO || RET=$?
+if [ $RET -eq 0 ]; then
+	echo "ERROR: Traverse verification may be wrong"
+fi
+RET=0
--- tests/05_test_statexfat.sh
+++ tests/05_test_statexfat.sh
@@ -2,6 +2,7 @@
 
 PROG=./statexfat
 IMAGE=exfat.img
+RET=0
 
 set -eu -o pipefail
 trap 'echo "ERROR: l.$LINENO, exit status = $?" >&2; exit 1' ERR
@@ -14,3 +15,38 @@ ${PROG} ${IMAGE} /4_FATCHAIN/FILE2.TXT
 ${PROG} --help
 ${PROG} --version
 ${PROG} -v ${IMAGE} /4_FATCHAIN/FILE2.TXT
+
+### Error path ###
+${PROG} ${IMAGE} 0 0 0 || RET=$?
+if [ $RET -eq 0 ]; then
+	echo "ERROR: Argument verification may be wrong"
+fi
+RET=0
+
+# Failure parse verification
+${PROG} -z ${IMAGE} / || RET=$?
+if [ $RET -eq 0 ]; then
+	echo "ERROR: Option Parser verification may be wrong"
+fi
+RET=0
+
+# Failure exist verification
+${PROG} nothing.img / || RET=$?
+if [ $RET -eq 0 ]; then
+	echo "ERROR: Open file verification may be wrong"
+fi
+RET=0
+
+# Failure exFAT image verification
+${PROG} README.md / || RET=$?
+if [ $RET -eq 0 ]; then
+	echo "ERROR: Image verification may be wrong"
+fi
+RET=0
+
+# Failure traverse verification
+${PROG} ${IMAGE} /NOTHING/NONE/NO || RET=$?
+if [ $RET -eq 0 ]; then
+	echo "ERROR: Traverse verification may be wrong"
+fi
+RET=0
