1f41f47e169ebf33758bff4f16228797027d6624
LeavaTail <starbow.duster@gmail.com>
2026-06-28 19:48:28 +0900

    [Improve] check: Update test

--- tests/01_test_checkexfat.sh
+++ tests/01_test_checkexfat.sh
@@ -4,6 +4,7 @@ PROG=./checkexfat
 IMAGE=exfat.img
 FAILURE_IMAGE=error.img
 RET=0
+OUT=
 
 set -eu -o pipefail
 trap 'echo "ERROR: l.$LINENO, exit status = $?" >&2; exit 1' ERR
@@ -11,6 +12,15 @@ trap 'echo "ERROR: l.$LINENO, exit status = $?" >&2; exit 1' ERR
 ### main function ###
 ${PROG} ${IMAGE}
 ${PROG} ${FAILURE_IMAGE}
+
+OUT=$(${PROG} ${IMAGE})
+echo "$OUT" | grep -q "Summary:"
+echo "$OUT" | grep -q "diagnostics: 0 error(s), 0 warning(s)"
+echo "$OUT" | grep -q "result: no issues found"
+
+OUT=$(${PROG} ${FAILURE_IMAGE})
+echo "$OUT" | grep -q "Summary:"
+echo "$OUT" | grep -q "result: issues found"
 
 ### Option function ###
 ${PROG} --help
